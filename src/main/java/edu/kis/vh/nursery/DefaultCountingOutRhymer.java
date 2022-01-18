9f56651435e44a3051877d74500bead4d3e3582b
Piotr <228714@edu.p.lodz.pl>
2022-01-18 13:22:18 +0100

    1.4 accesory 2  - Dodanie getter'a

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -10,6 +10,10 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[INT12];
 
 
+    public int getTotal() {
+        return total;
+    }
+
     private int total = TOTAL_VALUE;
 
     public void countIn(int in) {
